aa7a5eb6d28b056182064f15ac5d8f6c09acdbf8
Tastai44 <tastthai.654@gmail.com>
2026-01-31 21:41:27 +0700

    chore: Remove localhost from the CORS origin whitelist.

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-    origin: ['https://time-management-eta.vercel.app', 'http://localhost:3001'],
+    origin: ['https://time-management-eta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
